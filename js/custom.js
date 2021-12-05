dbbb025f0f1372a3283700f5ea7cb84c24ae8fc7
leon <leon@metag.ai>
2021-12-04 21:48:49 -0500

    add i18next to support multiple languages

--- js/custom.js
+++ js/custom.js
@@ -4,9 +4,7 @@
 			$('.spinner').fadeOut();
 			$('.preloader-area').delay(350).fadeOut('slow');
 		});
-
-		var logosrc = $('.brand');
-
+ 
 		$(window).on('scroll', function () {
 			if ($(window).scrollTop() > 120) {
 				var src = logosrc.attr("src").replace("logo.png", "logo_white.png");
@@ -19,6 +17,47 @@
 			}
 		});
 
+      
+         
+               i18next
+                       .use(i18nextBrowserLanguageDetector)
+                       .use(i18nextXHRBackend)
+                       .init({
+                         load: 'languageOnly',
+			 debug: true,
+                         fallbackLng: 'en',
+                         detection: {
+                           // order and from where user language should be detected
+                           order: ['querystring', 'cookie', 'localStorage', 'navigator', 'htmlTag'],
+                           // keys or params to lookup language from
+                           lookupQuerystring: 'lng',
+                           lookupCookie: 'i18next',
+                           lookupLocalStorage: 'i18nextLng',
+                           // cache user language on
+                           caches: ['localStorage', 'cookie'],
+                           excludeCacheFor: ['cimode'], // languages to not persist (cookie, localStorage)
+                           cookieMinutes: 10,
+                         },
+                         backend: {
+                           loadPath: 'locales/{{lng}}/{{ns}}.json',
+                         }
+                       }, function(err, t) {
+		         if (err) return console.log('something went wrong loading', err);
+                         jqueryI18next.init(i18next, $);
+                         $('nav').localize();
+                       });
+
+		$('a.lang').on("click", function (e) {
+			if (i18next.language == "en"){
+			i18next.changeLanguage("zh");
+			} else { i18next.changeLanguage("en")}
+                         $('nav').localize();
+			e.preventDefault();
+		});
+
+
+
+		var logosrc = $('.brand');
 		$(".testimonial-item").owlCarousel({
 			items: 2,
 			dots: true,
